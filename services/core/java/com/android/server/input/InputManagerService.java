486ee6e158e8a45a0fcffd2fea1dec362632ca6c
Chih-Wei Huang <cwhuang@linux.org.tw>
2014-05-18 02:20:32 +0800

    InputReader: read the pointercal from TSCalibration2
    Watch the pointercal and reload it on changed.

--- services/core/java/com/android/server/input/InputManagerService.java
+++ services/core/java/com/android/server/input/InputManagerService.java
@@ -63,6 +63,7 @@ import android.media.AudioManager;
 import android.os.Binder;
 import android.os.CombinedVibration;
 import android.os.Environment;
+import android.os.FileObserver;
 import android.os.Handler;
 import android.os.IBinder;
 import android.os.IVibratorStateListener;
@@ -453,6 +454,19 @@ public class InputManagerService extends IInputManager.Stub
         }
     }
 
+    private class TscalObserver extends FileObserver {
+        public TscalObserver() {
+            super("/data/misc/tscal/pointercal", CLOSE_WRITE);
+        }
+
+        @Override
+        public void onEvent(int event, String path) {
+            Slog.i(TAG, "detect pointercal changed");
+            reloadDeviceAliases();
+        }
+    }
+    private final TscalObserver mTscalObserver = new TscalObserver();
+
     public InputManagerService(Context context) {
         this(new Injector(context, DisplayThread.get().getLooper(), new UEventManager() {}));
     }
@@ -493,6 +507,7 @@ public class InputManagerService extends IInputManager.Stub
                 NAMESPACE_INPUT_NATIVE_BOOT, VELOCITYTRACKER_STRATEGY_PROPERTY);
 
         injector.registerLocalService(new LocalService());
+        mTscalObserver.startWatching();
     }
 
     public void setWindowManagerCallbacks(WindowManagerCallbacks callbacks) {
