c63e05b8b82847b764e93209edd1eeec5b3b0d05
nift4 <nift4@protonmail.com>
2023-04-23 09:56:46 +0200

    Implement transient task mode
    AKA "Tadi mode"
    
    Change-Id: I67a0ec743cd0084c2b63bdaf5aa78a20d269f47d

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -6295,6 +6295,11 @@ public final class Settings {
         public static final String AUTH_RIPPLE_ENABLED = "auth_ripple_enabled";
 
         /**
+         * @hide
+         */
+        public static final String TRANSIENT_TASK_MODE = "transient_task_mode";
+
+        /**
          * IMPORTANT: If you add a new public settings you also have to add it to
          * PUBLIC_SETTINGS below. If the new setting is hidden you have to add
          * it to PRIVATE_SETTINGS below. Also add a validator that can validate
@@ -6739,6 +6744,7 @@ public final class Settings {
             PRIVATE_SETTINGS.add(CLICK_PARTIAL_SCREENSHOT);
             PRIVATE_SETTINGS.add(KEY_EDGE_LONG_SWIPE_ACTION);
             PRIVATE_SETTINGS.add(SWAP_VOLUME_KEYS_ON_ROTATION);
+            PRIVATE_SETTINGS.add(TRANSIENT_TASK_MODE);
         }
 
         /**
@@ -6753,6 +6759,7 @@ public final class Settings {
             CLONE_TO_MANAGED_PROFILE.add(SOUND_EFFECTS_ENABLED);
             CLONE_TO_MANAGED_PROFILE.add(TEXT_SHOW_PASSWORD);
             CLONE_TO_MANAGED_PROFILE.add(TIME_12_24);
+            CLONE_TO_MANAGED_PROFILE.add(TRANSIENT_TASK_MODE);
         }
 
         /** @hide */
