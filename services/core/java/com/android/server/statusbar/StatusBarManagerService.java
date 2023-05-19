b7046340025eff979fc59da7cc533eef144e616b
nift4 <nift4@protonmail.com>
2023-05-19 20:26:46 +0200

    Porting menu button from pie to new pipeline
    Change-Id: Ice9d7eaf5b4067bec32af52820e9f80385200948

--- services/core/java/com/android/server/statusbar/StatusBarManagerService.java
+++ services/core/java/com/android/server/statusbar/StatusBarManagerService.java
@@ -601,15 +601,15 @@ public class StatusBarManagerService extends IStatusBarService.Stub implements D
         public void onSystemBarAttributesChanged(int displayId, @Appearance int appearance,
                 AppearanceRegion[] appearanceRegions, boolean navbarColorManagedByIme,
                 @Behavior int behavior, InsetsVisibilities requestedVisibilities,
-                String packageName, LetterboxDetails[] letterboxDetails) {
+                String packageName, LetterboxDetails[] letterboxDetails, boolean needsMenu) {
             getUiState(displayId).setBarAttributes(appearance, appearanceRegions,
                     navbarColorManagedByIme, behavior, requestedVisibilities, packageName,
-                    letterboxDetails);
+                    letterboxDetails, needsMenu);
             if (mBar != null) {
                 try {
                     mBar.onSystemBarAttributesChanged(displayId, appearance, appearanceRegions,
                             navbarColorManagedByIme, behavior, requestedVisibilities, packageName,
-                            letterboxDetails);
+                            letterboxDetails, needsMenu);
                 } catch (RemoteException ex) { }
             }
         }
@@ -1265,6 +1265,7 @@ public class StatusBarManagerService extends IStatusBarService.Stub implements D
         private int mImeWindowVis = 0;
         private int mImeBackDisposition = 0;
         private boolean mShowImeSwitcher = false;
+        private boolean mNeedsMenu = false;
         private IBinder mImeToken = null;
         private LetterboxDetails[] mLetterboxDetails = new LetterboxDetails[0];
 
@@ -1272,7 +1273,7 @@ public class StatusBarManagerService extends IStatusBarService.Stub implements D
                 AppearanceRegion[] appearanceRegions, boolean navbarColorManagedByIme,
                 @Behavior int behavior, InsetsVisibilities requestedVisibilities,
                 String packageName,
-                LetterboxDetails[] letterboxDetails) {
+                LetterboxDetails[] letterboxDetails, boolean needsMenu) {
             mAppearance = appearance;
             mAppearanceRegions = appearanceRegions;
             mNavbarColorManagedByIme = navbarColorManagedByIme;
@@ -1280,6 +1281,7 @@ public class StatusBarManagerService extends IStatusBarService.Stub implements D
             mRequestedVisibilities = requestedVisibilities;
             mPackageName = packageName;
             mLetterboxDetails = letterboxDetails;
+            mNeedsMenu = needsMenu;
         }
 
         private void showTransient(@InternalInsetsType int[] types) {
@@ -1416,7 +1418,7 @@ public class StatusBarManagerService extends IStatusBarService.Stub implements D
                     state.mImeBackDisposition, state.mShowImeSwitcher,
                     gatherDisableActionsLocked(mCurrentUserId, 2), state.mImeToken,
                     state.mNavbarColorManagedByIme, state.mBehavior, state.mRequestedVisibilities,
-                    state.mPackageName, transientBarTypes, state.mLetterboxDetails);
+                    state.mPackageName, transientBarTypes, state.mLetterboxDetails, state.mNeedsMenu);
         }
     }
 
