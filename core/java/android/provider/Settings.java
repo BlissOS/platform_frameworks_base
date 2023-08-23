47d07c49a48fc3171ac76c9fbbddf90bab411f31
hmtheboy154 <buingoc67@gmail.com>
2023-08-23 14:20:14 -0400

    Bypass restrictions on low RAM devices or Go builds [3/3]
    Change-Id: Iac400ed944c61c89a2e4e14d8cccfbb125d8d5d3
    Signed-off-by: hmtheboy154 <buingoc67@gmail.com>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -6820,6 +6820,11 @@ public final class Settings {
         public static final String PPU_SPOOF_BUILD_GMS_ARRAY = "ppu_spoof_build_gms_array";
 
         /**
+         * @hide
+         */
+        public static final String ALERT_WINDOW_BYPASS_LOW_RAM = "alert_window_bypass_low_ram";
+
+        /**
          * Keys we no longer back up under the current schema, but want to continue to
          * process when restoring historical backup datasets.
          *
