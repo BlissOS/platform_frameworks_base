e41101d1f504dda4236e4aa3e9a5b6b0fb2cc570
hmtheboy154 <buingoc67@gmail.com>
2023-08-23 14:20:14 -0400

    Bypass restrictions on low RAM devices or Go builds [3/3]
    Change-Id: Iac400ed944c61c89a2e4e14d8cccfbb125d8d5d3
    Signed-off-by: hmtheboy154 <buingoc67@gmail.com>
    Signed-off-by: Jackeagle <jackeagle102@gmail.com>
    
    Conflicts:
    	core/java/android/provider/Settings.java

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -7354,6 +7354,11 @@ public final class Settings {
         public static final String SCREENSHOT_SHUTTER_SOUND = "screenshot_shutter_sound";
 
         /**
+         * @hide
+         */
+        public static final String ALERT_WINDOW_BYPASS_LOW_RAM = "alert_window_bypass_low_ram";
+
+        /**
          * Keys we no longer back up under the current schema, but want to continue to
          * process when restoring historical backup datasets.
          *
