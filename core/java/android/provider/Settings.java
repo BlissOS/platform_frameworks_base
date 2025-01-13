9980d4a89e1615cf9319fb9872f0ebff39e9e90a
hmtheboy154 <buingoc67@gmail.com>
2025-01-13 10:53:29 -0500

    Revert "SystemUI: Add API for runtime taskbar config"
    This reverts commit 8fcbd379291a027ae3b4735965ef5dc75b4033f0.
    
    Change-Id: I0099a0d3158074862b35a39292b7f4c99d1f1adc

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -6905,11 +6905,6 @@ public final class Settings {
          * the setting value. See an example above.
          */
 
-        /**
-         * @hide
-         */
-        public static final String ENABLE_TASKBAR = "enable_taskbar";
-
 	/**
          * Enable/disable Bluetooth Battery bar
          * @hide
